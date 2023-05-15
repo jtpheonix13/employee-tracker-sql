5fe2b6fd07f7d88a76e82b7b7cebd51be7308290
jtpheonix13 <jgtihunt@comcast.net>
2023-05-15 17:35:51 -0500

    pushing up changes before stepping away for a bit

---
+++ utils/questions.js
@@ -0,0 +1,12 @@
+let departmentArray = [];
+let roleArray = [];
+let employeeArray = [];
+
+const startMenu = [
+    {
+        type: "list",
+        name: "startChoice",
+        message: "What would you like to do?",
+        choices: ["View all Departments", "View all Roles", "View all Employees", "Add Department", "Add Role", "Add Employee"]
+    }
+];
